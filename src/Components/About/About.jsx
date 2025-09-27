d11938497023867231f459c6c4bf6e5311913e8a
chethan <nchethan250@gmail.com>
2025-09-28 01:39:43 +0530

    Updated homepage design

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import profile_img from "../../assets/profile-img.png";
+import profile_img from "../../assets/profile_img.png";
 
 
 const About = () => {
